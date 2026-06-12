190f3c156e83086886ce105ea653fec2d6894eda
Filip Stevanovic <filip.stevanovic@elfak.rs>
2026-06-12 17:44:28 +0100

    Isus pomogi

---
+++ Deo II/Poljoprivredno gazdinstvo/Poljoprivredno gazdinstvo/Forme/Form_Subvencije.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poljoprivredno_gazdinstvo.Forme
+{
+    public partial class Form_Subvencije : Form
+    {
+        public Form_Subvencije()
+        {
+            InitializeComponent();
+        }
+    }
+}
